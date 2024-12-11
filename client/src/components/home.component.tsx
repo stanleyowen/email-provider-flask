760276588a4f21cf3df35d316c29bcc0d37666d4
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2024-12-12 01:15:34 +0800

    show reply feature

--- client/src/components/home.component.tsx
+++ client/src/components/home.component.tsx
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from "react";
 
 import { Refresh } from "../lib/icons.component";
 import axios from "axios";
-import { Tooltip, IconButton } from "@mui/material";
+import { Tooltip, IconButton, TextField, Grid2 } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+
 import { ChevronLeft, ChevronRight } from "../lib/icons.component";
 
 const Home = ({ auth, refreshInbox, parseError }: any) => {
@@ -10,6 +12,13 @@ const Home = ({ auth, refreshInbox, parseError }: any) => {
   const [viewMode, setViewMode] = useState<string | number>("list");
   const [selectedEmail, setSelectedEmail] = useState<any>(null);
   const [currentPage, setCurrentPage] = useState<number>(1);
+  const [replyMode, setReplyMode] = useState<boolean>(false);
+  const [isLoading, setLoading] = useState<boolean>(false);
+  const [data, setData] = useState<{ text: string }>({
+    text: "",
+  });
+
+  console.log(auth.emails);
   const emailsPerPage = 50;
 
   useEffect(() => {
@@ -26,6 +35,10 @@ const Home = ({ auth, refreshInbox, parseError }: any) => {
       setSelectedEmail(
         auth.emails.find((email: any) => email.seqno === target)
       );
+  };
+
+  const handleData = (key: string, value: string | number) => {
+    setData({ ...data, [key]: value });
   };
 
   const handleNextPage = () => {
@@ -58,6 +71,31 @@ const Home = ({ auth, refreshInbox, parseError }: any) => {
         setSelectedEmail(null);
         setViewMode("list");
       });
+  };
+
+  const replyEmail = async () => {
+    setLoading(true);
+
+    const credentials = JSON.parse(localStorage.getItem("credentials") || "{}");
+
+    await axios
+      .post(`${process.env.REACT_APP_API_URL}/mail/reply`, {
+        ...credentials,
+        to: selectedEmail.from.split(","),
+        subject: `Re: ${selectedEmail.subject}`,
+        text: data.text,
+        inReplyTo: selectedEmail.message_id || "",
+        references: selectedEmail.references || "",
+      })
+      .then((response) => {
+        parseError(response.data.message, true);
+      })
+      .catch((err) => {
+        parseError(err.response.data.message);
+      });
+
+    setLoading(false);
+    setReplyMode(false);
   };
 
   return (
@@ -145,6 +183,30 @@ const Home = ({ auth, refreshInbox, parseError }: any) => {
                 </button>
                 <button
                   className="card p-10 mb-10"
+                  style={{
+                    textAlign: "left",
+                    width: "auto",
+                    marginRight: "10px",
+                  }}
+                  disabled={selectedEmail.seqno === 1}
+                  onClick={() => switchMode(selectedEmail.seqno - 1)}
+                >
+                  Previous
+                </button>
+                <button
+                  className="card p-10 mb-10"
+                  style={{
+                    textAlign: "left",
+                    width: "auto",
+                    marginRight: "10px",
+                  }}
+                  disabled={selectedEmail.seqno === auth.emails.length}
+                  onClick={() => switchMode(selectedEmail.seqno + 1)}
+                >
+                  Next
+                </button>
+                <button
+                  className="card p-10 mb-10"
                   style={{ textAlign: "left", width: "auto" }}
                   onClick={() => deleteEmail(selectedEmail.seqno)}
                 >
@@ -158,11 +220,78 @@ const Home = ({ auth, refreshInbox, parseError }: any) => {
                   Date &nbsp;&nbsp;&nbsp;:{" "}
                   {new Date(selectedEmail.date).toDateString()}
                 </pre>
+
                 <div
                   dangerouslySetInnerHTML={{
                     __html: selectedEmail.body,
                   }}
                 ></div>
+
+                {!replyMode ? (
+                  <>
+                    <button
+                      className="card p-10 mt-10"
+                      style={{
+                        textAlign: "left",
+                        width: "auto",
+                        marginRight: "10px",
+                      }}
+                      disabled={auth.isLoading}
+                      onClick={() => setReplyMode(!replyMode)}
+                    >
+                      Reply
+                    </button>
+                    <button
+                      className="card p-10 mb-10"
+                      style={{
+                        textAlign: "left",
+                        width: "auto",
+                        marginRight: "10px",
+                      }}
+                      disabled={auth.isLoading}
+                    >
+                      Forward
+                    </button>
+                  </>
+                ) : (
+                  <></>
+                )}
+
+                {replyMode ? (
+                  <div>
+                    <div style={{ padding: "20px" }}>
+                      <h1 style={{ marginBottom: "10px" }}>Reply Email</h1>
+
+                      <Grid2 container spacing={3}>
+                        <Grid2 size={12}>
+                          <TextField
+                            label="Message"
+                            variant="outlined"
+                            className="w-100"
+                            value={data.text}
+                            required
+                            multiline
+                            rows={4}
+                            disabled={isLoading || auth.isLoading}
+                            onChange={(e) => handleData("text", e.target.value)}
+                          />
+                        </Grid2>
+
+                        <Grid2 size={12}>
+                          <LoadingButton
+                            variant="outlined"
+                            loading={isLoading || auth.isLoading}
+                            onClick={() => replyEmail()}
+                          >
+                            Send
+                          </LoadingButton>
+                        </Grid2>
+                      </Grid2>
+                    </div>
+                  </div>
+                ) : (
+                  <></>
+                )}
               </>
             ) : (
               <p>Email not found</p>
